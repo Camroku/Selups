3d0bf040eee77f51f54e957e4cb450d466aa9f33
Camroku <cinaryilmaz.gnu@gmail.com>
2022-06-17 15:56:40 +0300

    Initial

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static('static'))
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg, id) => {
+        io.emit('chat message', msg, id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
